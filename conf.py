5f21458f437072daede2b9238e3c508e9d8c3e23
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-07-04 16:35:36 +0200

    auto fetch the version for the doc

--- conf.py
+++ conf.py
@@ -11,6 +11,24 @@ from pathlib import Path
 
 from sphinx.locale import _
 
+
+def parse_version():
+
+    # read the file ".readthedocs.yaml" and parse to find all version numbers
+    with open(".readthedocs.yaml", "r") as f:
+        content = f.read()
+        versions = re.findall(r"(\d+\.\d+\.\d+)", content)
+
+    # make a commont version number which is the combination of the lowest values in the list
+    if versions:
+        major = min(int(v.split('.')[0]) for v in versions)
+        minor = min(int(v.split('.')[1]) for v in versions if int(v.split('.')[0]) == major)
+        patch = min(int(v.split('.')[2]) for v in versions if int(v.split('.')[0]) == major and int(v.split('.')[1]) == minor)
+        common_version = f"{major}.{minor}.{patch}"
+
+    return common_version
+
+
 def setup(app):
     app.add_css_file('custom.css')
 
@@ -23,7 +41,7 @@ project = 'clEsperanto'
 slug = re.sub(r"\W+", "-", project.lower())
 author = 'clEsperanto authors'
 copyright = f"2020-{year}, clEsperanto authors"
-release = '0.16.0'
+release = parse_version()
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -42,6 +60,7 @@ extensions = [
     'sphinx_inline_tabs',
     'breathe',
     'sphinxemoji.sphinxemoji',
+    'sphinx.ext.graphviz',
 ]
 
 templates_path = ['_templates']
@@ -83,6 +102,12 @@ pygments_dark_style = "monokai"
 
 html_static_path = ['_static']
 
+html_css_files = [
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/fontawesome.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/solid.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/brands.min.css",
+]
+
 html_theme_options = {
     "light_logo": "logo_w.svg",
     "dark_logo": "logo_d.svg",
@@ -90,13 +115,9 @@ html_theme_options = {
     "footer_icons": [
         {
             "name": "GitHub",
-            "url": "https://github.com/clEsperanto",
-            "html": """
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-                </svg>
-            """,
-            "class": "",
+            "url": "https://github.com/clEsperanto/",
+            "html": "",
+            "class": "fa-brands fa-solid fa-github fa-2x",
         },
     ],
 }
